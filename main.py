5c77f7dcf978da177eb3623771cf56f3dc0259b4
alirezayazdan813 <128980212+alirezayazdan813@users.noreply.github.com>
2023-05-04 17:02:25 +0330

    50% of main

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from warehouse import warehouse
+from Accountancy-class import Accountancy-class
+from Order-class import Order-class 
+
+print("Welcome")
+print("How do you want to enter the shop?")
+print("1.as a customer 2.as a manager")
+a = input()
+if a == 1:
+    print("I will treat you as a customer    ?????????")
+
+elif a == 2:
+    print("What do you want to do?  1.inventory 2.accounting")
+    b = input()
+    if b == 1:
+        print("Which part of warehousing are you interested in? ")
+        print("1.show warehouse inventory")
+        print("2.updating inventory")
+        print("3.adding new products")
+        c = input ()
+        #      ....      ?
+
+
+    elif b == 2:
+        print("       connecting to accounting      ???")
+    
 
