7de84b16a3784d309fcad672a2fcefdef49b8bdb
ErfanDehghanD <126862894+ErfanDehghanD@users.noreply.github.com>
2023-05-06 21:01:18 +0330

    v.2 of accountancy class contain 2 methods

--- accountancy/accountance.py
+++ accountancy/accountance.py
@@ -3,12 +3,6 @@ import os
 
 class Accountancy:
 
-    def init_file(self):
-        output = open('accountancy_output.csv', 'x')
-        result = "product count, order id, total price, delivery price, tax"
-        output.write(result)
-        output.close()
-
     def make_file(self, product_count, order_number, product_price, delivery_price):
         output = open('accountancy_output.csv', 'a')
         result = f'{product_count}, {order_number}, {float(product_price) * int(product_count)}, {delivery_price},' \
