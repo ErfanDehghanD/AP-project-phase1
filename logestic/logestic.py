70e195c9cdf1416452fa03b6ad9387377c352c87
alirezayazdan813 <128980212+alirezayazdan813@users.noreply.github.com>
2023-05-06 06:56:29 +0330

    edit checking and optioning delivery time

--- logestic/logestic.py
+++ logestic/logestic.py
@@ -1,15 +1,10 @@
-# hello logestics
-print(" hello logestics ")
-print(" It's a nice day don't you agree ")
-print(" whuaaaa ")
-
 # determining cities of the counties
 # 1=> tehran = 1   (tehran county just have a city)
 # 2=> esphahan = 1 / ghahjavarestan = 2
 # 3=> tabriz = 1 / sardorud = 2
 
 # where these datas come from ??????????????????????????
-class Logestics:
+class logestic:
     def __init__(self, county, city, address, postal_code):
         self.county = county
         self.city = city
@@ -21,24 +16,36 @@ class Logestics:
 
     def county_check(self):
         if self.county == 1 :
-            print("It will be delivered by courier")
+            return "courier"
         else :
-            print("It will be posted")
+            return "post"
         
     # morining, noon and evening
-    # what about the day ?????????????????
-    # which one is in priority to check if its full ... ??????????????????????
-    def delivery_time(self):
-        if self.ncounter <= 3 :
-            self.ncounter += 1
-            print("It will be delivered at? noon")
 
-        elif self.ecounter <= 3:
+    def available_time(self):
+        if self.ncounter <= 3 and self.ecounter <= 3:
+            print("when do you want your order to be delivered ?  1.morning  2.noon 3.evening   :    ")
+
+        elif self.ecounter <= 3 and self.ncounter > 3:
+            print("when do you want your order to be delivered ?  1.morning  3.evening   :    ")
+
+        elif self.ncounter <= 3 and self.ecounter > 3:
+            print("when do you want your order to be delivered ?  1.morning  2.noon   :    ")
+
+        elif self.ncounter > 3 and self.ecounter > 3 :
+            print("when do you want your order to be delivered ?  1.morning      :      ")
+
+    def delivery_time(self, delivery_time):
+        if delivery_time == "noon" :
+            self.ncounter += 1
+            print("It will be delivered at noon")
+
+        elif delivery_time == "evening":
             self.ecounter += 1
             print("It will be delivered in the evening")
 
         else :
-            print("It will be delivered in? morning")
+            print("It will be delivered in the morning")
         
 
     
